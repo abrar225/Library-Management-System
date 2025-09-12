be001924728833991fa59e9bc16e874e193f168e
Abrar Akhunji <84306460+abrar225@users.noreply.github.com>
2025-09-12 04:15:22 -0700

    settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -124,10 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-# STATIC_ROOT = BASE_DIR / 'static'
-
 # settings.py (at the bottom)
 
 STATIC_URL = 'static/'
