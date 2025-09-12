fe5e5336eefc16bc6a735cf9a60ca511ded90a41
Abrar Akhunji <84306460+abrar225@users.noreply.github.com>
2025-09-12 07:46:21 -0700

    settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -28,7 +28,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://library-management-system-1-pqfh.onrender.com']
+# settings.py
+
+ALLOWED_HOSTS = [
+    'library-management-system-1-pqfh.onrender.com', # <-- ADD THIS NEW URL
+    'library-management-system-pyq5.onrender.com',   # <-- Keep the old one too
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
